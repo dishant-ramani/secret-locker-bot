ee6ed012dd2139bebcd325feebda7f9acb85f1de
Dishant-Ramani <dramani987@rku.ac.in>
2026-04-10 17:18:49 +0530

    config updated

--- config.php
+++ config.php
@@ -4,27 +4,20 @@
    TELEGRAM BOT CONFIGURATION
    ============================== */
 
-// REPLACE THIS with your Telegram Bot Token
+// Telegram Bot Token
 $bot_token = "8774149133:AAF505151Jbu_Am6Nfo8szSH_7oCoaUdjSE";
 
-// Telegram API URL (Do not change)
+// Telegram API URL
 $api_url = "https://api.telegram.org/bot".$bot_token;
 
 
 /* ==============================
-   DATABASE CONFIGURATION
+   DATABASE CONFIGURATION (Railway)
    ============================== */
 
-// REPLACE THIS with your Database Host
-$db_host = "sql207.infinityfree.com";
-
-// REPLACE THIS with your Database Username
-$db_user = "if0_41616616";
-
-// REPLACE THIS with your Database Password
-$db_pass = "w7dAl4WNPD";
-
-// REPLACE THIS with your Database Name
-$db_name = "if0_41616616_vault";
+$db_host = getenv("${{RAILWAY_PRIVATE_DOMAIN}}");
+$db_user = getenv("root");
+$db_pass = getenv("MYSQL_ROOTZOMZTpNOLKNKHbuIdqqpkCXpLLgYwoiQ_PASSWORD");
+$db_name = getenv("railway");
 
 ?>
